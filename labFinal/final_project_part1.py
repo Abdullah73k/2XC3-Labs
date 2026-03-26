645990360db7c3634dd6434e65ec112c05a967da
Abdullah Khamis <abdullahbkhamis@gmail.com>
2026-03-25 21:16:14 -0400

    completed shortest path approx algos

--- labFinal/final_project_part1.py
+++ labFinal/final_project_part1.py
@@ -1,6 +1,7 @@
 import min_heap
 import random
 
+
 class DirectedWeightedGraph:
 
     def __init__(self):
@@ -33,41 +34,43 @@ class DirectedWeightedGraph:
 
 
 def dijkstra(G, source):
-    pred = {} #Predecessor dictionary. Isn't returned, but here for your understanding
-    dist = {} #Distance dictionary
+    pred = {}  # Predecessor dictionary. Isn't returned, but here for your understanding
+    dist = {}  # Distance dictionary
     Q = min_heap.MinHeap([])
     nodes = list(G.adj.keys())
 
-    #Initialize priority queue/heap and distances
+    # Initialize priority queue/heap and distances
     for node in nodes:
         Q.insert(min_heap.Element(node, float("inf")))
         dist[node] = float("inf")
     Q.decrease_key(source, 0)
 
-    #Meat of the algorithm
+    # Meat of the algorithm
     while not Q.is_empty():
         current_element = Q.extract_min()
         current_node = current_element.value
         dist[current_node] = current_element.key
         for neighbour in G.adj[current_node]:
             if dist[current_node] + G.w(current_node, neighbour) < dist[neighbour]:
-                Q.decrease_key(neighbour, dist[current_node] + G.w(current_node, neighbour))
+                Q.decrease_key(
+                    neighbour, dist[current_node] + G.w(current_node, neighbour)
+                )
                 dist[neighbour] = dist[current_node] + G.w(current_node, neighbour)
                 pred[neighbour] = current_node
     return dist
 
 
 def bellman_ford(G, source):
-    pred = {} #Predecessor dictionary. Isn't returned, but here for your understanding
-    dist = {} #Distance dictionary
+    pred = {}  # Predecessor dictionary. Isn't returned, but here for your understanding
+    dist = {}  # Distance dictionary
     nodes = list(G.adj.keys())
 
-    #Initialize distances
+    # Initialize distances
     for node in nodes:
         dist[node] = float("inf")
     dist[source] = 0
 
-    #Meat of the algorithm
+    # Meat of the algorithm
     for _ in range(G.number_of_nodes()):
         for node in nodes:
             for neighbour in G.adj[node]:
@@ -77,33 +80,88 @@ def bellman_ford(G, source):
     return dist
 
 
+def dijkstra_approx(G: DirectedWeightedGraph, source, k: int):
+    nodes = list(G.adj.keys())
+    dist = {}
+    relaxes = {}
+    Q = min_heap.MinHeap([])
+    
+    for node in nodes: 
+        Q.insert(min_heap.Element(node, float("inf")))
+        dist[node] = float("inf")
+        relaxes[node] = k
+    Q.decrease_key(source, 0)
+    while not Q.is_empty():
+        curr_el = Q.extract_min()
+        curr_node = curr_el.value
+        dist[curr_node] = curr_el.key
+        
+        for neighbor in G.adj[curr_node]:
+            if relaxes[neighbor] > 0 and dist[neighbor] > (weight := dist[curr_node] + G.w(curr_node, neighbor)):
+                dist[neighbor] = weight
+                relaxes[neighbor] = relaxes[neighbor] - 1
+                Q.decrease_key(neighbor, weight)
+                
+    return dist
+    
+
+
+def bellman_ford_approx(G: DirectedWeightedGraph, source, k: int):
+    nodes = list(G.adj.keys())
+    relaxes = {}
+    dist = {}
+    updated = False
+
+    for node in nodes:
+        dist[node] = float("inf")
+        relaxes[node] = k
+    dist[source] = 0
+
+    for _ in range(G.number_of_nodes() - 1):
+        updated = False
+        for node in nodes:
+            for neighbor in G.adj[node]:
+                updated = True
+                if relaxes[neighbor] > 0 and dist[neighbor] > (
+                    new := dist[node] + G.w(node, neighbor)
+                ):
+                    dist[neighbor] = new
+                    relaxes[neighbor] = relaxes[neighbor] - 1
+        if not updated:
+            break
+        
+    return dist
+
+
 def total_dist(dist):
     total = 0
     for key in dist.keys():
         total += dist[key]
     return total
 
-def create_random_complete_graph(n,upper):
+
+def create_random_complete_graph(n, upper):
     G = DirectedWeightedGraph()
     for i in range(n):
         G.add_node(i)
     for i in range(n):
         for j in range(n):
             if i != j:
-                G.add_edge(i,j,random.randint(1,upper))
+                G.add_edge(i, j, random.randint(1, upper))
     return G
 
 
-#Assumes G represents its nodes as integers 0,1,...,(n-1)
+# Assumes G represents its nodes as integers 0,1,...,(n-1)
 def mystery(G):
     n = G.number_of_nodes()
     d = init_d(G)
     for k in range(n):
         for i in range(n):
             for j in range(n):
-                if d[i][j] > d[i][k] + d[k][j]: 
+                if d[i][j] > d[i][k] + d[k][j]:
                     d[i][j] = d[i][k] + d[k][j]
     return d
+
 
 def init_d(G):
     n = G.number_of_nodes()
